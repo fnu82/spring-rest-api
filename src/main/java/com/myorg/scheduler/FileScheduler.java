9efb7a0457f7ae5280ae035d502cb495a7dbf27a
Utpal <futpal@k12.enterprise.adm>
2017-04-11 15:35:13 -0400

    updated scheduler

--- src/main/java/com/myorg/scheduler/FileScheduler.java
+++ src/main/java/com/myorg/scheduler/FileScheduler.java
@@ -1,12 +1,28 @@
 package com.myorg.scheduler;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 @Component
 public class FileScheduler {
-	@Scheduled(fixedRate = 1000000)
-    public void reportCurrentTime() {
-       
+	@Scheduled(fixedRate = 10000)
+  	public void lastFileModified() {
+		Path file = Paths.get("c://users//futpal//test"); // change to match local directory
+		Stream<File> recentFile=Arrays.stream(file.toFile().listFiles()).
+		                                    filter(f-> f.isFile() && (System.currentTimeMillis() - f.lastModified()) < 10000);
+		                                   
+		List<File> fileList=recentFile.collect(Collectors.toList());
+		if(!CollectionUtils.isEmpty(fileList)){
+			System.out.println("send an email");
+		}
     }
 }
